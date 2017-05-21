fe28749d06cd4ac42bd3ad7e12b7eecbf450735d
darjwx <darjwx@gmail.com>
2017-05-21 22:36:18 +0200

    Quarks: add static launcher shortcuts
    * minSdkVersion 25. Shortcuts are supported from that version.
    * Shortcuts: Incognito tab, favourites and new tab.
    
    Change-Id: I8e60d43b5a51282e40e7cd41dc714a9e104aca87

---
+++ app/src/main/java/org/carbonrom/quarks/shortcuts/Incognito.java
@@ -0,0 +1,17 @@
+package org.carbonrom.quarks.shortcuts;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import org.carbonrom.quarks.MainActivity;
+
+public class Incognito extends Activity{
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       Context context = getApplicationContext();
+       MainActivity.handleShortcuts(context, "incognito");
+   }
+}
