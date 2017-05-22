16d4f818fa854bffbb07aa13c37cfc35ec1508f9
Joe Maples <joe@frap129.org>
2017-05-22 16:21:02 -0400

    Quarks: QuickTiles for Favorites, Incognito, and New Tabs
    Change-Id: Iad7900805b0bc13de65b93589dab05409ec14463
    Signed-off-by: Joe Maples <joe@frap129.org>

--- app/src/main/java/org/carbonrom/quarks/MainActivity.java
+++ app/src/main/java/org/carbonrom/quarks/MainActivity.java
@@ -718,4 +718,22 @@ public class MainActivity extends WebViewExtActivity implements View.OnTouchList
             mSwipeRefreshLayout.setEnabled(true);
         }
     }
+
+    // Intents used for QuickTiles and other shortcuts
+    public static boolean handleShortcuts(Context c, String shortcut) {
+        switch (shortcut){
+            case "incognito":
+                Intent intent = new Intent(c, MainActivity.class);
+                intent.putExtra(EXTRA_INCOGNITO, true);
+                c.startActivity(intent);
+                break;
+            case "newtab":
+                c.startActivity(new Intent(c, MainActivity.class));
+                break;
+            case "favorites":
+                c.startActivity(new Intent(c, FavoriteActivity.class));
+                break;
+        }
+        return true;
+    }
 }
