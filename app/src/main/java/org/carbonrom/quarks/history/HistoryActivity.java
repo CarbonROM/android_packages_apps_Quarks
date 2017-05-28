da035f00fa949fa5688298bae034d84e3ff27264
Gabriele M <moto.falcon.git@gmail.com>
2017-05-28 12:09:15 +0200

    Quarks: Query an already ordered history
    While at it, switch to a readable database.
    
    Change-Id: I719a09a35ef0c7c2873fa36be12abbf77f853d2a
    Signed-off-by: Joe Maples <joe@frap129.org>

--- app/src/main/java/org/carbonrom/quarks/history/HistoryActivity.java
+++ app/src/main/java/org/carbonrom/quarks/history/HistoryActivity.java
@@ -34,7 +34,6 @@ import org.carbonrom.quarks.R;
 import org.carbonrom.quarks.utils.UiUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -112,8 +111,6 @@ public class HistoryActivity extends AppCompatActivity {
 
     void refresh() {
         List<HistoryItem> items = mDbHandler.getAllItems();
-        // Reverse database list order
-        Collections.reverse(items);
         mAdapter.updateList(items);
 
         if (items.isEmpty()) {
