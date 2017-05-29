3e75f09aa604c82ed16d8f9c2a094c1a8229047f
Luca Stefani <luca020400@lineageos.org>
2017-05-29 17:21:32 +0200

    Quarks: Add HTTP auth login support
    Change-Id: I9c3fc72de09736c25337b723c55eb5c186d1e8ec

--- app/src/main/java/org/carbonrom/quarks/webview/WebClient.java
+++ app/src/main/java/org/carbonrom/quarks/webview/WebClient.java
@@ -33,44 +33,67 @@ import java.util.Map;
 
 class WebClient extends WebViewClient {
 
-    private Map<String, Boolean> loadedUrls = new HashMap<>();
+	private Map<String, Boolean> loadedUrls = new HashMap<>();
 
-    WebClient() {
-        super();
-    }
+	WebClient() {
+		super();
+	}
 
-    @Override
-    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        String url = request.getUrl().toString();
-        Context context = view.getContext();
-        if (!url.startsWith("http")) {
-            try {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(request.getUrl());
-                context.startActivity(intent);
-            } catch (ActivityNotFoundException e) {
-                Snackbar.make(view, context.getString(R.string.error_no_activity_found),
-                        Snackbar.LENGTH_LONG).show();
-            }
-            return true;
-        }
+	@Override
+	public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+		String url = request.getUrl().toString();
+		Context context = view.getContext();
+		if (!url.startsWith("http")) {
+			try {
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(request.getUrl());
+				context.startActivity(intent);
+			} catch (ActivityNotFoundException e) {
+				Snackbar.make(view, context.getString(R.string.error_no_activity_found),
+						Snackbar.LENGTH_LONG).show();
+			}
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-        boolean ad;
-        if (!PrefsUtils.getAdBlocker(view.getContext()))
-            return super.shouldInterceptRequest(view, request);
-        String url = request.getUrl().toString();
-        if (!loadedUrls.containsKey(url)) {
-            ad = AdBlocker.isAd(url);
-            loadedUrls.put(url, ad);
-        } else {
-            ad = loadedUrls.get(url);
-        }
-        return ad ? AdBlocker.createEmptyResource() :
-                super.shouldInterceptRequest(view, request);
-    }
+	@Override
+	public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+		boolean ad;
+		if (!PrefsUtils.getAdBlocker(view.getContext()))
+			return super.shouldInterceptRequest(view, request);
+		String url = request.getUrl().toString();
+		if (!loadedUrls.containsKey(url)) {
+			ad = AdBlocker.isAd(url);
+			loadedUrls.put(url, ad);
+		} else {
+			ad = loadedUrls.get(url);
+		}
+		return ad ? AdBlocker.createEmptyResource() :
+				super.shouldInterceptRequest(view, request);
+	}
+
+	@Override
+	public void onReceivedHttpAuthRequest(WebView view,
+										  HttpAuthHandler handler, String host, String realm) {
+		Context context = view.getContext();
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		LayoutInflater layoutInflater = LayoutInflater.from(context);
+		View dialogView = layoutInflater.inflate(R.layout.auth_dialog, new LinearLayout(context));
+		EditText username = (EditText) dialogView.findViewById(R.id.username);
+		EditText password = (EditText) dialogView.findViewById(R.id.password);
+		TextView auth_detail = (TextView) dialogView.findViewById(R.id.auth_detail);
+		String text = context.getString(R.string.auth_dialog_detail, view.getUrl());
+		auth_detail.setText(text);
+		builder.setView(dialogView)
+				.setCancelable(false)
+				.setTitle(R.string.auth_dialog_title)
+				.setPositiveButton(R.string.auth_dialog_login,
+						(dialog, whichButton) -> handler.proceed(
+								username.getText().toString(), password.getText().toString()))
+				.setNegativeButton(android.R.string.cancel,
+						(dialog, whichButton) -> handler.cancel())
+				.show();
+	}
 }
