54c05e4f75814c75d1e9213e0a8d8ad7c06feb3d
Luca Stefani <luca020400@lineageos.org>
2017-06-24 22:16:31 +0200

    Quarks: Add save form data setting
    * Enabled by default
    
    Change-Id: I373a521a9ff9094f97ca961b91cb64f4be8dd478

--- app/src/main/java/org/carbonrom/quarks/utils/PrefsUtils.java
+++ app/src/main/java/org/carbonrom/quarks/utils/PrefsUtils.java
@@ -33,6 +33,7 @@ public final class PrefsUtils {
     private static final String KEY_SUGGESTION_PROVIDER = "key_suggestion_provider";
     private static final String KEY_INCOGNITO_POLICY = "key_incognito_policy";
     private static final String KEY_ADBLOCKER = "key_adblocker";
+    private static final String KEY_SAVE_FORM_DATA = "key_save_form_data";
 
     public enum SuggestionProviderType {
         BAIDU,
@@ -108,6 +109,11 @@ public final class PrefsUtils {
     public static boolean getAdBlocker(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getBoolean(KEY_ADBLOCKER, true);
+    }
+
+    public static boolean getSaveFormData(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(KEY_SAVE_FORM_DATA, true);
     }
 
     public static void setHomePage(Context context, String value) {
