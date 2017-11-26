a15b7d6cd23c6a971115a075e5d904925e6a9bdf
Alexandre Oliveira <rockytvbr@gmail.com>
2017-11-26 16:42:40 -0200

    Quarks: add dialog to show SSL certificate info
    * Based off the same functionality from Soren Stoutner's Privacy Browser
      (https://git.stoutner.com/?p=PrivacyBrowser.git)
    
    * Preview: https://goo.gl/DPYfUQ
    
    Change-Id: I46d568ed794183f94f41dc5e5a4543df7917eb37
    (cherry picked from commit 77b808b)
    Signed-off-by: CertifiedBlyndGuy <certifiedblyndguy@gmail.com>

--- app/src/main/java/org/carbonrom/quarks/webview/WebClient.java
+++ app/src/main/java/org/carbonrom/quarks/webview/WebClient.java
@@ -74,7 +74,7 @@ class WebClient extends WebViewClient {
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
-        mUrlBarController.onPageLoadFinished();
+        mUrlBarController.onPageLoadFinished(view.getContext(), view.getCertificate());
     }
 
     @Override
